e42729798b8d3868a62028e963a59bf33b7e8829
mrafalak <maciej.rafalak.dev@gmail.com>
2025-03-12 23:37:39 +0100

    [MRFIT-12] Create exercise updaters

--- feature/exercise/data/src/main/java/com/mrapps/data/repository/ExerciseRepositoryImpl.kt
+++ feature/exercise/data/src/main/java/com/mrapps/data/repository/ExerciseRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.mrapps.data.repository
 
+import com.mrapps.data.manager.exercise_update.ExerciseUpdateManager
 import com.mrapps.data.local.dao.exercise.ExerciseDao
 import com.mrapps.data.local.dao.exercise.type.endurance.EnduranceExerciseDao
 import com.mrapps.data.local.dao.exercise.type.strength.StrengthExerciseDao
@@ -27,6 +28,7 @@ class ExerciseRepositoryImpl @Inject constructor(
     private val exerciseDao: ExerciseDao,
     private val strengthExerciseDao: StrengthExerciseDao,
     private val enduranceExerciseDao: EnduranceExerciseDao,
+    private val exerciseUpdateManager: ExerciseUpdateManager,
     private val dispatchers: DispatcherProvider
 ) : ExerciseRepository {
 
@@ -99,72 +101,7 @@ class ExerciseRepositoryImpl @Inject constructor(
         saved: ExerciseEntity,
         new: Exercise
     ): Result<Unit, DataError.Local> {
-        return when (new.type.type) {
-            ExerciseTypeEnum.STRENGTH -> updateStrengthExercise(saved, new)
-            ExerciseTypeEnum.ENDURANCE -> updateEnduranceExercise(saved, new)
-        }
-    }
-
-    private suspend fun updateStrengthExercise(
-        saved: ExerciseEntity,
-        new: Exercise
-    ): Result<Unit, DataError.Local> {
-        val mappingResult =
-            safeMappingOperation<Pair<ExerciseEntity, StrengthExerciseEntity>, ExerciseRepositoryImpl> {
-                new.toExerciseWithStrengthEntities()
-            }
-
-        val (exerciseEntity, strengthEntity) = when (mappingResult) {
-            is Result.Success -> mappingResult.data
-            is Result.Error -> return Result.Error(mappingResult.error)
-        }
-
-        return if (saved.type == new.type.type) {
-            safeDatabaseOperation<Unit, ExerciseRepositoryImpl> {
-                exerciseDao.updateExerciseWithStrengthExercise(
-                    exercise = exerciseEntity,
-                    strengthExercise = strengthEntity
-                )
-            }
-        } else {
-            safeDatabaseOperation<Unit, ExerciseRepositoryImpl> {
-                exerciseDao.removeSavedExerciseAndAddNewStrengthExercise(
-                    exercise = exerciseEntity,
-                    strengthExercise = strengthEntity
-                )
-            }
-        }
-    }
-
-    private suspend fun updateEnduranceExercise(
-        saved: ExerciseEntity,
-        new: Exercise
-    ): Result<Unit, DataError.Local> {
-        val mappingResult =
-            safeMappingOperation<Pair<ExerciseEntity, EnduranceExerciseEntity>, ExerciseRepositoryImpl> {
-                new.toExerciseWithEnduranceEntities()
-            }
-
-        val (exerciseEntity, enduranceEntity) = when (mappingResult) {
-            is Result.Success -> mappingResult.data
-            is Result.Error -> return Result.Error(mappingResult.error)
-        }
-
-        return if (saved.type == new.type.type) {
-            safeDatabaseOperation<Unit, ExerciseRepositoryImpl> {
-                exerciseDao.updateExerciseWithEnduranceExercise(
-                    exercise = exerciseEntity,
-                    enduranceExercise = enduranceEntity
-                )
-            }
-        } else {
-            safeDatabaseOperation<Unit, ExerciseRepositoryImpl> {
-                exerciseDao.removeSavedExerciseAndAddNewEnduranceExercise(
-                    exercise = exerciseEntity,
-                    enduranceExercise = enduranceEntity
-                )
-            }
-        }
+        return exerciseUpdateManager.updateExercise(saved, new)
     }
 
     private suspend fun getStrengthExercise(exerciseEntity: ExerciseEntity): Result<Exercise, DataError.Local> {
